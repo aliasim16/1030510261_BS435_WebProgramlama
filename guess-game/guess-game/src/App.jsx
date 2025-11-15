31d661816ec80cb0746df0ed78052b56ae3111cb
Ali Asım <aliasim1625@hotmail.com>
2025-11-15 16:14:33 +0300

    guess game başlangıc

--- guess-game/guess-game/src/App.jsx
+++ guess-game/guess-game/src/App.jsx
@@ -7,13 +7,7 @@ function App() {
 
     return (
         <>
-            <h1>{count}</h1>
-            <Counter count1={count} setCount1={setCount}/>
-            <Counter count1={count} setCount1={setCount}/>
-            <Counter count1={count} setCount1={setCount}/>
-            <Counter count1={count} setCount1={setCount}/>
-            <Counter count1={count} setCount1={setCount}/>
-            <Counter count1={count} setCount1={setCount}/>
+
         </>
     )
 }
