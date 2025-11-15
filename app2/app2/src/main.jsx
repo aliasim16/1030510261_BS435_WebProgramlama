35ba664f9a2bfaaf68a4fed9963dd7c3e5cae08a
Ali Asım <aliasim1625@hotmail.com>
2025-11-15 15:53:39 +0300

    app2

--- app2/app2/src/main.jsx
+++ app2/app2/src/main.jsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {AuthProvider} from "./AuthContext.tsx";
+import {BrowserRouter} from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const container = document.getElementById('root');
+if (container) {
+  createRoot(container).render(
+    <StrictMode>
+        <AuthProvider>
+            <BrowserRouter>
+              <App />
+            </BrowserRouter>
+        </AuthProvider>
+    </StrictMode>
+  );
+}
