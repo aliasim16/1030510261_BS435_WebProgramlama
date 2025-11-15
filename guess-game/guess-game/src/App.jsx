4ba6c882382ac36bc18b86cfb3bd5f2c33d3e960
Ali Asım <aliasim1625@hotmail.com>
2025-11-15 13:57:39 +0300

    app2 initial

--- guess-game/guess-game/src/App.jsx
+++ guess-game/guess-game/src/App.jsx
@@ -1,24 +1,21 @@
 import './App.css'
-import { Counter } from './counter';
-import { useState } from 'react'
-
+import {Counter} from "./counter";
+import {useState} from "react";
 
 function App() {
+    const [count, setCount] = useState(10);
 
-  const [count,setCount]=useState(5);
-
-  return (
-    <>
-    <h1>{count}</h1>
-    <Counter count1={count} setCount1={setCount}/>
-    <Counter count1={count} setCount1={setCount}/>
-    <Counter count1={count} setCount1={setCount}/>
-    <Counter count1={count} setCount1={setCount}/>
-    <Counter count1={count} setCount1={setCount}/>
-    <Counter count1={count} setCount1={setCount}/>
-      
-    </>
-  )
+    return (
+        <>
+            <h1>{count}</h1>
+            <Counter count1={count} setCount1={setCount}/>
+            <Counter count1={count} setCount1={setCount}/>
+            <Counter count1={count} setCount1={setCount}/>
+            <Counter count1={count} setCount1={setCount}/>
+            <Counter count1={count} setCount1={setCount}/>
+            <Counter count1={count} setCount1={setCount}/>
+        </>
+    )
 }
 
 export default App
--- guess-game/guess-game/src/counter.jsx
+++ guess-game/guess-game/src/counter.jsx
@@ -1,26 +1,24 @@
-import { useState } from "react";
+import {useState} from "react";
 
+export const Counter = ({count1, setCount1}) => {
+    const [count, setCount] = useState(0)
 
-export const Counter=({count1,setCount1})=>{
-
-    const[count,setCount]=useState(0);
-
-    return(
-        <>
-        <h1>{count}</h1>
-        <div className="card">
-            <button onClick={() => {
-                setCount(count + 1);
-                setCount1(count1 - 1);
-            } }>Increase</button>
-            <button onClick={() => {
-                setCount(count - 1);
-                setCount1(count1 + 1);
-            } }>Decrease</button>
-
-        </div>
-        
+    return( <>
+            <h1>{count}</h1>
+            <div className="card">
+                <button onClick={() => {
+                    setCount((count) => count + 1)
+                    setCount1((count1) => count1 + 1)
+                }}>
+                    Artır
+                </button>
+                <button onClick={() => {
+                    setCount((count) => count - 1)
+                    setCount1((count1) => count1 - 1)
+                }}>
+                    Azalt
+                </button>
+            </div>
         </>
- 
     )
 }
