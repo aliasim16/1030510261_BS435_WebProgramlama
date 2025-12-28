ad3f0348fd87a1114010371fce3ea0bd256771d8
Ali Asım <aliasim1625@hotmail.com>
2025-12-28 16:30:01 +0300

    tailwind bootstrap

--- guess-game/guess-game/src/main.jsx
+++ guess-game/guess-game/src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { GameProvider } from './context/GameContext';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css"; 
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
